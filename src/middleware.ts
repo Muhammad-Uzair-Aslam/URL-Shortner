88b40f883474bc89f4284ddd9b90f7142cedcc1c
Uzair Aslam <uzairaslam990@gmail.com>
2025-03-02 09:34:22 +0400

    protected pages from unauthenticated user

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const authToken = request.cookies.get("next-auth.session-token"); // 🔥 NextAuth session token
-
-  const isPublicPath = ["/", "/signin", "/signup", "/password/forgot", "/password/reset"].includes(path);
+  const isPublicPath = [
+    "/",
+    "/signin",
+    "/signup",
+    "/password/forgot",
+    "/password/reset",
+  ].includes(path);
   const isProtectedPath = ["/dashboard", "/profile", "/edit"].includes(path);
 
   if (authToken && isPublicPath) {
@@ -13,17 +18,8 @@ export async function middleware(request: NextRequest) {
   if (!authToken && isProtectedPath) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
-
   return NextResponse.next();
 }
-
 export const config = {
-  matcher: [
-    "/",
-    "/dashboard",
-    "/signin",
-    "/signup",
-    "/profile",
-    "/edit",
-  ],
+  matcher: ["/", "/dashboard", "/signin", "/signup", "/profile", "/edit"],
 };
